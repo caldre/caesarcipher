c9786676696e4337306d1de48efb643878d64fae
Jyrki Riestola <jyrki.riestola@gmail.com>
2020-06-18 03:35:18 +0300

    Changed intro text

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,23 +7,16 @@ const Header = () => {
       <h1>DO YOU EVEN FINNISH?!</h1>
       <br></br>
       <p>
-        So here's the common problem we all have. Your friend just sent you a
-        message, but it's full of gibberish. Knowing your friends strange habits
-        you immediately presume he has encrypted the message. This application
-        lets you decrypt it, as long as it is Finnish and using Caesar cipher.
-        Enjoy!
-        <br></br>
-        <br></br>
-        Wanna try it for yourself?
-        <br></br>
-        <a
-          href="https://cryptii.com/pipes/caesar-cipher"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CLICK ME!
-        </a>
+        This app lets you crypt a message using simple Caesar cipher (with
+        Finnish alphabets). It's originated from a code challenge I did a while
+        back, but they took down their message-API, so I had to make some other
+        use for this <br></br>¯\_(ツ)_/¯<br></br>
+        P.S Autosolve-button is the real prize here - it runs some tests on
+        message and decides if it's Finnish! Sadly I made some "imporovements"
+        and kinda broke it. Try to test it, or sample the code instead for
+        getting better picture
       </p>
+      <br></br>
       <Input />
     </header>
   );
